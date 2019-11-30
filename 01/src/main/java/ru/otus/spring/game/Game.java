392d298b0c59398a2c684c5e7c9dfb615693ee4d
chocosin <jake96785@gmail.com>
2019-11-30 13:48:22 +0600

    hw1 ask for name

--- 01/src/main/java/ru/otus/spring/game/Game.java
+++ 01/src/main/java/ru/otus/spring/game/Game.java
@@ -1,6 +1,10 @@
 package ru.otus.spring.game;
 
+import java.text.MessageFormat;
+import java.util.regex.Pattern;
+
 public class Game {
+    private static final Pattern NAME_PATTERN = Pattern.compile("\\w+ \\w+");
     private final GameConfig gameConfig;
     private final Player player;
 
@@ -10,12 +14,15 @@ public class Game {
     }
 
     public void play() {
+        String name = askForName();
+
         int correctAnswers = 0;
         int numberOfQuestions = gameConfig.getQuestionCount();
         if (numberOfQuestions == 0) {
             player.showResult("У нас к вам нет вопросов");
             return;
         }
+
         for (int questionInd = 0; questionInd < numberOfQuestions; questionInd++) {
             Question question = gameConfig.getQuestion(questionInd);
             String playerAnswer = player.askQuestion(question.getQuestion());
@@ -24,7 +31,17 @@ public class Game {
             }
         }
 
-        String result = "Количество правильных ответов: " + correctAnswers;
+        String result = MessageFormat.format("Количество правильных ответов: {0}. Спасибо за игру, {1}.", correctAnswers, name);
         player.showResult(result);
+    }
+
+    private String askForName() {
+        while (true) {
+            String name = player.askQuestion("Введите ваши имя и фамилию (например, \"Владимир Ленин\")").trim();
+            if (NAME_PATTERN.matcher(name).matches()) {
+                return name;
+            }
+            player.showHint("Имя должно быть в формате \"Имя Фамилия\"");
+        }
     }
 }
